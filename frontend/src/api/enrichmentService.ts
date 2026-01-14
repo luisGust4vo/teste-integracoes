e72cee14c2c02680b3eab7f969250fac97c3855d
Luis <lluisresende13@gmail.com>
2026-01-13 23:34:25 -0300

    Feat:Criacao das rotas para o consumo do front end

---
+++ frontend/src/api/enrichmentService.ts
@@ -0,0 +1,9 @@
+import { EnrichedUser } from "../types/user";
+
+const API_B = import.meta.env.VITE_API_B_URL;
+
+export async function getEnrichedUser(uuid: string): Promise<EnrichedUser> {
+  const res = await fetch(`${API_B}/users/enriched/${uuid}`);
+  if (!res.ok) throw new Error("Dados ainda não enriquecidos");
+  return res.json();
+}
