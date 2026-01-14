39abdc1b5feabe6c056fc67a8fcc25abd5c0cc4a
Luis <lluisresende13@gmail.com>
2026-01-14 00:32:00 -0300

    Feat:Criacao dos servicoes externos e configuracoes,tambem configuracao da migration e model

--- user-service/app/Models/User.php
+++ user-service/app/Models/User.php
@@ -18,9 +18,9 @@ class User extends Authenticatable
      * @var list<string>
      */
     protected $fillable = [
+        'uuid',
         'name',
         'email',
-        'password',
     ];
 
     /**
