2f951ae0eb5221df454f7622e286c7fe60f0fde8
Luis <lluisresende13@gmail.com>
2026-01-13 23:35:44 -0300

    Feat:Criacao do arquivo de listagem

---
+++ frontend/src/pages/UserList.tsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+import { getUsers } from "../api/userService";
+import { User } from "../types/user";
+import { useNavigate } from "react-router-dom";
+
+export default function UserList() {
+  const [users, setUsers] = useState<User[]>([]);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    getUsers().then(setUsers).catch(console.error);
+  }, []);
+
+  return (
+    <div>
+      <h1>Usuários</h1>
+      <button onClick={() => navigate("/users/new")}>Novo Usuário</button>
+
+      <ul>
+        {users.map(user => (
+          <li key={user.id}>
+            {user.name} - {user.email}
+            <button onClick={() => navigate(`/users/${user.id}`)}>
+              Ver detalhes
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
