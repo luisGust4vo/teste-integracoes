45ac215df318aec5fd4fcb5473a23dac0571dee3
Luis <lluisresende13@gmail.com>
2026-01-15 18:50:13 -0300

    Feat: Mudanca para arquivo de  rota api

--- user-service/routes/web.php
+++ user-service/routes/web.php
@@ -6,6 +6,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::post('/users', [UserController::class, 'store']);
-Route::get('/users', [UserController::class, 'index']);
-Route::get('/users/{id}', [UserController::class, 'show']);
+
