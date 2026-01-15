8bfc4ec08e192719d96dad04cb6c0a7c5e4aa800
Luis <lluisresende13@gmail.com>
2026-01-15 18:47:55 -0300

    Feat: Mudanca para criacao de usuario e evento na fila

--- user-service/app/Http/Controllers/UserController.php
+++ user-service/app/Http/Controllers/UserController.php
@@ -3,41 +3,48 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Services\RabbitMQService;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\DB;
 class UserController extends Controller
 {
-    public function store(Request $request)
-        {
-            $validated = $request->validate([
-                'name' => 'required|min:3',
-                'email' => 'required|email|unique:users,email',
-            ]);
 
-            $user = User::create([
-                'uuid' => Str::uuid(),
-                'name' => $validated['name'],
-                'email' => $validated['email'],
-            ]);
 
-            // Publicar evento na fila (vem no próximo passo)
+public function store(Request $request, RabbitMQService $rabbit)
+{
+    $validated = $request->validate([
+        'name' => 'required|min:3',
+        'email' => 'required|email|unique:users,email',
+    ]);
 
-            return response()->json($user, 201);
-        }
+    DB::beginTransaction();
 
-        public function index()
-        {
-            return User::all();
-        }
+    try {
+        $user = User::create([
+            'uuid' => Str::uuid(),
+            'name' => $validated['name'],
+            'email' => $validated['email'],
+        ]);
 
-        public function show($id)
-        {
-            $user = User::find($id);
+        $rabbit->publish([
+            'event' => 'user.created',
+            'uuid' => $user->uuid,
+            'name' => $user->name,
+        ]);
 
-            if (!$user) {
-                return response()->json(['message' => 'Usuário não encontrado'], 404);
-            }
+        DB::commit();
 
-            return $user;
-        }
+        return response()->json($user, 201);
+
+    } catch (\Throwable $e) {
+        DB::rollBack();
+
+        return response()->json([
+            'message' => 'Erro ao criar usuário'
+        ], 500);
+    }
+}
+
 
 }
+
